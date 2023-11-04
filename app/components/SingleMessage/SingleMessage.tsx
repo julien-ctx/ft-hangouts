ec7ed2abbc3609a215c76abf8427dc3dcbb485a4
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-11-04 11:47:04 +0100

    feat: add timestamps in message

--- app/components/SingleMessage/SingleMessage.tsx
+++ app/components/SingleMessage/SingleMessage.tsx
@@ -4,6 +4,7 @@ import { Message } from "./SingleMessage.typing"
 import { colors } from "../../utils/theme/colors"
 import { ColorContext } from "../../providers/color/ColorContext"
 import { spacing } from "../../utils/theme/spacing"
+import { formatDateStringMessage } from "../../utils/format/date"
 
 interface Props {
   message: Message
@@ -17,6 +18,10 @@ export const SingleMessage = ({ message }: Props) => {
   const textColor = message.isReceived
     ? colors.palette.black
     : colors.palette.white
+  const timestampAlignment = message.isReceived ? "left" : "right"
+  const timestampColor = message.isReceived
+    ? colors.palette.grey500
+    : colors.palette.grey200
   return (
     <View
       style={[
@@ -26,6 +31,14 @@ export const SingleMessage = ({ message }: Props) => {
     >
       <Text style={[styles.messageText, { color: textColor }]}>
         {message.content}
+      </Text>
+      <Text
+        style={[
+          styles.timestamp,
+          { textAlign: timestampAlignment, color: timestampColor },
+        ]}
+      >
+        {formatDateStringMessage(new Date(message.timestamp))}
       </Text>
     </View>
   )
@@ -38,9 +51,15 @@ const styles = StyleSheet.create({
     paddingVertical: spacing.sm,
     borderRadius: 10,
     maxWidth: "85%",
+    flexDirection: "column",
   },
   messageText: {
     color: colors.palette.black,
     fontSize: 16,
+    marginBottom: spacing.md,
+  },
+  timestamp: {
+    fontSize: 11,
+    fontStyle: "italic",
   },
 })
--- app/utils/format/date.ts
+++ app/utils/format/date.ts
@@ -8,3 +8,13 @@ export const formatDateString = (date: Date) => {
 
   return `${month}/${day}/${year} - ${hours}:${minutes}:${seconds}`
 }
+
+export const formatDateStringMessage = (date: Date) => {
+  const day = String(date.getDate()).padStart(2, "0")
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const year = date.getFullYear()
+  const hours = String(date.getHours()).padStart(2, "0")
+  const minutes = String(date.getMinutes()).padStart(2, "0")
+
+  return `${month}/${day}/${year} - ${hours}:${minutes}`
+}
